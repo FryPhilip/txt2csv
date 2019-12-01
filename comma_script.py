9a6f3a64bc45436855c447020221b149d26eb6d3
jonas <jonas_ott@outlook.com>
2019-12-01 19:24:16 +0100

    works and is done

--- comma_script.py
+++ comma_script.py
@@ -1,17 +1,19 @@
 f = open('ext_index.csv', 'r')
 fr = open('out.csv', 'w')
 
+counter = 0
+
 for str in f.read():
-    counter = 0
-    for chrr in str:
-        if chrr.isspace():
-            fr.write(",")
-            counter += 1
-        else:
-            fr.write(chrr)
-        if counter >= 13:
-            print(counter)
-            fr.write("\n")
+    if str.isspace():
+        fr.write(",")
+        counter += 1
+    else:
+        fr.write(str)
+    if counter >= 13:
+        print(counter)
+        fr.write("\n")
+        counter = 0
+
         
 f.close()
 fr.close()
