cf53341d2f6dd696d0db42015e1c2e60dbf0c938
jonas <jonas_ott@outlook.com>
2019-12-02 12:12:06 +0100

    final commit

--- comma_script.py
+++ comma_script.py
@@ -1,19 +1,26 @@
-f = open('ext_index.csv', 'r')
-fr = open('out.csv', 'w')
+#open streams
+f = open('ext_index.csv', 'r') 
+fr = open('out.csv', 'w')   
+
+##Number of colums on source file.
+print("Enter number of colums:")
+input(colum_num) 
 
 counter = 0
 
+## Interates through the input file.
 for str in f.read():
-    if str.isspace():
-        fr.write(",")
+    if str.isspace(): 
+        fr.write(",") ##writes comma to output 
         counter += 1
     else:
-        fr.write(str)
-    if counter >= 13:
+        fr.write(str) ##writes current character to output
+    if counter >= colum_num: ##checks for line break through number of colums
         fr.write("\n")
         counter = 0
 
-fr.write(",")
-        
+fr.write(",") ##comma at the end of file
+
+#close streams      
 f.close()
 fr.close()
